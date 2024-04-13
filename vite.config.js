69bdb65138b48041b260ef8c64fe63eeb570a5b1
Thomas Santoyo <tommysantoyo@gmail.com>
2024-04-12 22:12:31 -0500

    Project layout and links done

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
+import { resolve } from 'path';
+import Sitemap from 'vite-plugin-sitemap'
+import handlebars from 'vite-plugin-handlebars';
+
 export default {
     build: {
         outDir: 'docs',
-        emptyOutDir: true
-    }
+        emptyOutDir: true,
+        rollupOptions: {
+            input: [
+                'index.html',
+                'about.html',
+                'contact.html',
+                'gallery.html',
+                'services.html',
+                'service-area.html',
+                '404.html'],
+        }
+    },
+    plugins: [
+        Sitemap({
+            outDir: 'docs',
+            hostname: 'https://santoyodesign.com'
+        }),
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials'),
+            context: {
+                theme: 'emerald',
+            },
+        })
+    ],
 };
