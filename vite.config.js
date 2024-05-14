aa079c2c3e56d67c5e16fbd6edf91873186e752e
Thomas Santoyo <tommysantoyo@gmail.com>
2024-05-13 21:34:14 -0500

    fix gallery hrefs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { resolve } from 'path';
 import Sitemap from 'vite-plugin-sitemap'
 import handlebars from 'vite-plugin-handlebars';
+import fs from 'fs';
+import path from 'path';
 
 export default {
     build: {
@@ -27,6 +29,28 @@ export default {
             context: {
                 theme: 'emerald',
             },
+            helpers: {
+                resolveAssetUrl: (url) => {
+                    // Read the content of the dist directory
+                    const distDir = fs.readdirSync('docs/assets');
+
+                    // Get the file name without the directory path and the extension
+                    const fileName = path.basename(url, path.extname(url));
+
+                    console.log(fileName)
+
+                    // Find the file that matches the original image name
+                    const compiledFileName = distDir.find(file => file.includes(fileName));
+
+                    // If a matching file is found, return the new file name
+                    if (compiledFileName) {
+                        return `/assets/${compiledFileName}`;
+                    }
+
+                    // If no matching file is found, return the original url
+                    return url;
+                }
+            }
         })
     ],
 };
